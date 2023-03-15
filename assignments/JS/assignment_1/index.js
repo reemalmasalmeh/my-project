7b9b641b24befc8cb6b8c94040be64c18e1fc628
Reem Almasalmeh <reem.almasalmeh@gmail.com>
2023-03-15 14:38:26 -0700

    js assignment1

---
+++ assignments/JS/assignment_1/index.js
@@ -0,0 +1,34 @@
+
+
+window.onload = function () {
+    assignment_1.querySelector("h1").style.color = "Blue" ;
+    };
+
+    // single comment
+
+    /* multiple commen
+    comment
+    comment*/
+
+    console.log("log");
+    console.table(["abdullah", "wesam", "reem"]);
+    console.log("%c ELZERO WEB %c SCHOOL", "color: blue ; font-size: 40px", "color: red ; font-size: 40px");
+    console.log("reem mas");
+    console.log(typeof "reem mas");
+    console.log(typeof 5000);
+    console.log(typeof 50,100);
+    console.log(typeof [10,20,30]);
+    console.log(typeof ["osa,ree"]);
+    console.log(typeof { name:'reem' , age:25 , country:'syria' } );
+    console.log( typeof true);
+    console.log( typeof false);
+    console.log( typeof undefined);
+    console.log( typeof null);
+    var user = "reem";
+    var age = "25";
+    console.log( user);
+    console.log( user);
+    console.log( user);
+    console.log( age);
+    console.log( age);
+    console.log( "hello");
---
+++ assignments/JS/main.js
@@ -0,0 +1,34 @@
+window.onload = function () {
+document.querySelector("h1").style.color = "Blue" ;
+};
+
+// single comment
+
+/* multiple commen
+comment
+comment*/
+window.alert("hello js"); //this alert is notalways used because ofalot of her problems
+document.write("<h2>Hello<h2>");
+console.log("log");
+console.error("error");
+console.table(["abdullah", "wesam", "reem"]);
+console.log("%c hello in %c js file", "color: blue ; font-size: 40px", "color: red ; font-size: 40px");
+console.log("reem mas");
+console.log(typeof "reem mas");
+console.log(typeof 5000);
+console.log(typeof 50,100);
+console.log(typeof [10,20,30]);
+console.log(typeof ["osa,ree"]);
+console.log(typeof { name:'reem' , age:25 , country:'syria' } );
+console.log( typeof true);
+console.log( typeof false);
+console.log( typeof undefined);
+console.log( typeof null);
+var user = "reem";
+var age = "25";
+console.log( user);
+console.log( user);
+console.log( user);
+console.log( age);
+console.log( age);
+console.log( "hello");
