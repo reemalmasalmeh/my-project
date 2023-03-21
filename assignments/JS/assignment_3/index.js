ea3fc9cc28c50a10a9c638c66f8995229b1f3bee
Reem Almasalmeh <reem.almasalmeh@gmail.com>
2023-03-20 17:48:55 -0700

    assignment-3

---
+++ assignments/JS/assignment_3/index.js
@@ -0,0 +1,49 @@
+let num = 3;
+a = true;
+
+// Solution One
+console.log(num*2 ); // 6
+
+// Solution Two
+console.log(num + a*num); // 6
+
+// Soultion Three
+console.log(a*6); // 6
+
+// Soultion Four
+console.log(a + a + a + a + a + a); // 6
+
+// Solution Five
+console.log(6 ** a); // 6
+
+// Solution Six
+console.log(num**2 - num); // 6
+
+
+let number = "10";
+
+// Solution One
+console.log(number*2); // 20
+
+// Solution Two
+console.log(number/2 * 4); // 20
+
+// Solution Three
+console.log(number % 6 * 5 ); // 20
+
+// Solution Four
+console.log(number ** 2 - 80); // 20
+
+
+let points = 10;
+
+// Write Your Code Here
+points += 3;
+
+console.log( points ); // 13
+
+// Write Your Code Here
+points -= 5;
+
+
+console.log(points); // 8;
